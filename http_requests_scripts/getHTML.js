1dc1476a83d4b232ad76654bf21106ff79115feb
Paulyn <paulyntran@live.ca>
2019-02-11 19:37:54 +0000

    Adding step 4 of http requests scripts activity

---
+++ http_requests_scripts/getHTML.js
@@ -0,0 +1,28 @@
+var requestOptions = {
+    host: 'syntantris.github.io',
+    path: '/http-examples/step4.html'
+};
+
+var https = require('https');   
+
+
+function getHTML (options, callback) {
+
+    https.get(options, function (response) {
+        // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+
+        response.on('data', function(chunk) {
+                    // console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+                    console.log(chunk.toString());
+                });
+  
+  });
+}
+
+function printHTML (html) {
+    console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
+
